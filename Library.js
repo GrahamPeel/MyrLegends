8deeae4ef2bc2035e3199e21f294cfe9c3a292e7
Graham Peel <graham.peel@gmail.com>
2019-07-01 22:53:17 -0400

    Variable number of colums in GameSheet

--- Library.js
+++ Library.js
@@ -1,1 +1,12 @@
 ﻿
+
+function SaveGameObj(gameObj) {
+    localStorage.setItem('SingleSavedGame', JSON.stringify(gameObj));
+}
+
+function GetGameObj() {
+    var storedJson = localStorage.getItem('SingleSavedGame');
+    return (!!storedJson) ? JSON.parse(storedJson) : null;
+}
+
+// Game Obj = { heroCount: [int], savedItems: [string of decoded form input vals] }
